6db26d190f986a61ad862bf59e963acff3a73df9
Cong Vu <vuquoccong01@gmail.com>
2022-08-18 09:09:30 +0700

    Chapter 3,4,5

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,6 +1,3 @@
 class ApplicationJob < ActiveJob::Base
-  # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
-  # Most jobs are safe to ignore if the underlying records are nolongeravailable
-  # discard_on ActiveJob::DeserializationError
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
+
+  get "/help", to: "static_pages#help" 
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
+
+  root_path = "/" 
+  root_url = "http://www.example.com/"
+  help_path = "/help"
+  help_url = "http://www.example.com/help"
+  about_path = "/about"
+  about_url = "http://www.example.com/about"
+  contact_path = "/contact"
+  contact_url = "http://www.example.com/contact"
+
+  signup_path = "/signup"
 end
